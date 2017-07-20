3dc6e78afc43fa4681babdb65be9949444db647c
Nervviks <Nervvikss@gmail.com>
2017-07-20 16:03:02 +0300

    a

--- test10/src/test10/main.java
+++ test10/src/test10/main.java
@@ -3,8 +3,8 @@ package test10;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 	TODO Auto-generated method stub
+		System.out.println("master");
 	}
 
 }
